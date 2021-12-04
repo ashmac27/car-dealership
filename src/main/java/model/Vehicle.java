82804541b47e427f11207605e3eeaa84afeea719
shawnguerrabautista <shawnguerrabautista@gmail.com>
2021-12-04 18:44:20 -0500

    finished vehicle model

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -1,5 +1,152 @@
 package model;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+
 public class Vehicle {
 
+    private String VIN;
+    private int makeId;
+    private int modelId;
+    private String type;
+    private String bodyStyle;
+    private int year;
+    private String transmission;
+    private String color;
+    private String interior;
+    private int mileage;
+    private BigDecimal salePrice;
+    private BigDecimal msrp;
+    private String description;
+    private boolean isFeatured;
+    private boolean isSold;
+    private Blob picture;
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
+
+    public int getMakeId() {
+        return makeId;
+    }
+
+    public void setMakeId(int makeId) {
+        this.makeId = makeId;
+    }
+
+    public int getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(int modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBodyStyle() {
+        return bodyStyle;
+    }
+
+    public void setBodyStyle(String bodyStyle) {
+        this.bodyStyle = bodyStyle;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getInterior() {
+        return interior;
+    }
+
+    public void setInterior(String interior) {
+        this.interior = interior;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public BigDecimal getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(BigDecimal salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public BigDecimal getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(BigDecimal msrp) {
+        this.msrp = msrp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isFeatured() {
+        return isFeatured;
+    }
+
+    public void setFeatured(boolean featured) {
+        isFeatured = featured;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
+
+    public Blob getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Blob picture) {
+        this.picture = picture;
+    }
 }
