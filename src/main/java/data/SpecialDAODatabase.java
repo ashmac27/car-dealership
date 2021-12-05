7570fbb942b1533ed9fdadd509e8bdac9cd94530
shawnguerrabautista <shawnguerrabautista@gmail.com>
2021-12-05 17:29:25 -0500

    added missing annotation

--- src/main/java/data/SpecialDAODatabase.java
+++ src/main/java/data/SpecialDAODatabase.java
@@ -1,15 +1,16 @@
 package data;
 
 import model.Special;
-import model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class SpecialDAODatabase implements SpecialDAO {
 
     @Autowired
