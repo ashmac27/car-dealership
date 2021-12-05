87397e9774dd8f57935875a423c1204ee3504519
shawnguerrabautista <shawnguerrabautista@gmail.com>
2021-12-05 16:58:59 -0500

    added special dao

--- src/main/java/data/SpecialDAO.java
+++ src/main/java/data/SpecialDAO.java
@@ -1,4 +1,20 @@
 package data;
 
+import model.Special;
+
+import java.util.List;
+
 public interface SpecialDAO {
+
+    // Gets special by id
+    Special getSpecialById(int specialId);
+
+    // Gets list of all special
+    List<Special> getSpecialList();
+
+    // Adds special to database
+    Special addSpecial(Special special);
+
+    // Deletes special from database
+    boolean deleteSpecial(int specialId);
 }
--- src/main/java/data/SpecialDAODatabase.java
+++ src/main/java/data/SpecialDAODatabase.java
@@ -1,4 +1,64 @@
 package data;
 
-public class SpecialDAODatabase {
+import model.Special;
+import model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SpecialDAODatabase implements SpecialDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcTemp;
+
+    @Override
+    public Special getSpecialById(int specialId) {
+        final String SELECT_SPECIAL = "SELECT * " +
+                "FROM specials " +
+                "WHERE SpecialId = ?";
+        Special special = jdbcTemp.queryForObject(SELECT_SPECIAL, new SpecialMapper(), specialId);
+        return special;
+    }
+
+    @Override
+    public List<Special> getSpecialList() {
+        final String SELECT_LIST_OF_SPECIAL = "SELECT * " +
+                "FROM specials";
+        List<Special> specialList = jdbcTemp.query(SELECT_LIST_OF_SPECIAL, new SpecialMapper());
+        return specialList;
+    }
+
+    @Override
+    public Special addSpecial(Special special) {
+        final String ADD_SPECIAL = "INSERT INTO specials " +
+                "(SpecialId, Title, Description) " +
+                "VALUES (?, ?, ?)";
+        jdbcTemp.update(ADD_SPECIAL, special.getSpecialId(), special.getTitle(), special.getDescription());
+        int newId = jdbcTemp.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        special.setSpecialId(newId);
+        return special;
+    }
+
+    @Override
+    public boolean deleteSpecial(int specialId) {
+        final String DELETE_SPECIAL = "DELETE FROM specials " +
+                "WHERE SpecialId = ?";
+        return jdbcTemp.update(DELETE_SPECIAL, specialId) > 0;
+    }
+
+    public static final class SpecialMapper implements RowMapper<Special> {
+
+        @Override
+        public Special mapRow(ResultSet resultSet, int i) throws SQLException {
+            Special special = new Special();
+            special.setSpecialId(resultSet.getInt("SpecialId"));
+            special.setTitle(resultSet.getString("Title"));
+            special.setDescription(resultSet.getString("Description"));
+            return special;
+        }
+    }
 }
